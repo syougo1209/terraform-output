43bf027c906932d16b8a18f788ff1a3af279d8b3
尾崎しょうご <syougo1999most@yahoo.co.jp>
2022-02-22 12:01:54 +0900

    create ecs and db

--- security_group.tf
+++ security_group.tf
@@ -21,3 +21,19 @@ module "http_redirect_sg" {
   port        = 8080
   cidr_blocks = ["0.0.0.0/0"]
 }
+
+module "nginx_sg" {
+  source      = "./security_group"
+  name        = "nginx-sg"
+  vpc_id      = aws_vpc.aisk_prd.id
+  port        = 80
+  cidr_blocks = [aws_vpc.aisk_prd.cidr_block]
+}
+
+module "mysql_sg" {
+  source      = "./security_group"
+  name        = "mysql-sg"
+  vpc_id      = aws_vpc.aisk_prd.id
+  port        = 3306
+  cidr_blocks = [aws_vpc.aisk_prd.cidr_block]
+}
